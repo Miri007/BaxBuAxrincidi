e1f8974387004b12141de064d9cd7640035dfa10
Miri007 <84359280+Miri007@users.noreply.github.com>
2021-05-28 10:37:50 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 que = {}
-SESSION_NAME = getenv("SESSION_NAME", "session")
+SESSION_NAME = getenv("1850550311", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
 admins = {}
